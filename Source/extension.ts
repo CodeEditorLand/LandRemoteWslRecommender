5222edcd25e9a595e4f0778006b6eff1f452bf9f
Nikola Hristov <nikola@nikolahristov.tech>
2023-10-30 12:23:38 +0200

    squash!

--- Source/extension.ts
+++ Source/extension.ts
@@ -1,50 +1,64 @@
 /*---------------------------------------------------------------------------------------------
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *--------------------------------------------------------------------------------------------*/
-import * as vscode from 'vscode';
-import * as os from 'os';
-import * as path from 'path';
-import * as fs from 'fs';
-import * as cp from 'child_process';
-import { promisify } from 'util';
+import * as vscode from "vscode";
+import * as os from "os";
+import * as path from "path";
+import * as fs from "fs";
+import * as cp from "child_process";
+import { promisify } from "util";
 
-import * as nls from 'vscode-nls';
-import { Experiment, Command, getTelemetry, Dialog } from './telemetry';
+import * as nls from "vscode-nls";
+import { Experiment, Command, getTelemetry, Dialog } from "./telemetry";
 
 const localize = nls.loadMessageBundle();
 
 export enum ExtensionId {
-	remoteWSL = 'ms-vscode-remote.remote-wsl',
-	remoteWSLRecommender = 'ms-vscode-remote.remote-wsl-recommender'
+	remoteWSL = "ms-vscode-remote.remote-wsl",
+	remoteWSLRecommender = "ms-vscode-remote.remote-wsl-recommender",
 }
 
 enum ContextKey {
-	connectToWSL = 'remote-wsl-recommender.connectToWSL.available',
-	gettingStarted = 'remote-wsl-recommender.gettingStarted.available'
+	connectToWSL = "remote-wsl-recommender.connectToWSL.available",
+	gettingStarted = "remote-wsl-recommender.gettingStarted.available",
 }
 
 enum CommandId {
-	connectToWSL = 'remote-wsl-recommender.connectToWSL',
-	gettingStarted = 'remote-wsl-recommender.gettingStarted'
+	connectToWSL = "remote-wsl-recommender.connectToWSL",
+	gettingStarted = "remote-wsl-recommender.gettingStarted",
 }
 
-const WIN10_1903 = { label: 'Windows 10, May 2019 Update, version 1903', build: 18362 };
+const WIN10_1903 = {
+	label: "Windows 10, May 2019 Update, version 1903",
+	build: 18362,
+};
 //const WIN10_2004 = { label: 'Windows 10, May 2020 Update, version 2004', build: 19041 };
 
-const WSL_DOC_URL = 'https://code.visualstudio.com/docs/remote/wsl';
+const WSL_DOC_URL = "https://code.visualstudio.com/docs/remote/wsl";
 
 export async function activate(context: vscode.ExtensionContext) {
-	if (process.platform !== 'win32' || getWindowsBuildNumber() < WIN10_1903.build || vscode.env.remoteName) {
+	if (
+		process.platform !== "win32" ||
+		getWindowsBuildNumber() < WIN10_1903.build ||
+		vscode.env.remoteName
+	) {
 		return;
 	}
 
 	const telemetry = getTelemetry(context);
 
-	async function addFeature(experiment: Experiment, contextKey: ContextKey, initFeature: () => vscode.Disposable) {
+	async function addFeature(
+		experiment: Experiment,
+		contextKey: ContextKey,
+		initFeature: () => vscode.Disposable
+	) {
 		let featureDisposable: vscode.Disposable | undefined = undefined;
 
 		const isFeatureEnabled = async () => {
-			return !vscode.extensions.getExtension(ExtensionId.remoteWSL) && await telemetry.isExperimentEnabled(experiment);
+			return (
+				!vscode.extensions.getExtension(ExtensionId.remoteWSL) &&
+				(await telemetry.isExperimentEnabled(experiment))
+			);
 		};
 
 		let isEnabled = false;
@@ -52,7 +66,11 @@ export async function activate(context: vscode.ExtensionContext) {
 			const newIsEnabled = await isFeatureEnabled();
 			if (isEnabled !== newIsEnabled) {
 				isEnabled = newIsEnabled;
-				vscode.commands.executeCommand('setContext', contextKey, isEnabled);
+				vscode.commands.executeCommand(
+					"setContext",
+					contextKey,
+					isEnabled
+				);
 				if (isEnabled) {
 					featureDisposable = initFeature();
 				} else if (featureDisposable) {
@@ -62,68 +80,123 @@ export async function activate(context: vscode.ExtensionContext) {
 			}
 		};
 		await featureEnablementChanged();
-		context.subscriptions.push(vscode.extensions.onDidChange(featureEnablementChanged));
-		context.subscriptions.push(telemetry.onDidChange(featureEnablementChanged));
-		context.subscriptions.push({ dispose: () => featureDisposable?.dispose() });
+		context.subscriptions.push(
+			vscode.extensions.onDidChange(featureEnablementChanged)
+		);
+		context.subscriptions.push(
+			telemetry.onDidChange(featureEnablementChanged)
+		);
+		context.subscriptions.push({
+			dispose: () => featureDisposable?.dispose(),
+		});
 	}
 
 	addFeature(Experiment.connectToWSL, ContextKey.connectToWSL, () => {
-		return vscode.commands.registerCommand(CommandId.connectToWSL, async () => {
-			telemetry.reportCommand(Command.connectToWSL);
-			const isWSLInstalled = await checkIfWSLInstalled();
-			if (isWSLInstalled !== true) {
-				telemetry.reportDialog(Dialog.wslNotInstalled, 'show');
-				const installWSL = 'Install Now';
-				const learnMore = 'Learn More';
-				const powershellLocation = getPowershellLocation();
-				const buttons = (hasWSLInstall() && powershellLocation) ? [installWSL, learnMore] : [learnMore];
-				const response = await vscode.window.showErrorMessage(localize('installWSL', 'The Windows Subsystem for Linux (WSL) must be installed to complete the action. WSL lets you run a GNU/Linux environment directly on Windows without the overhead of a traditional virtual machine. VS Code, through the WSL extension, can then open folders and run commands, extensions, and the terminal in WSL.'), ...buttons);
-				if (response === learnMore) {
-					telemetry.reportDialog(Dialog.wslNotInstalled, 'open');
-					await vscode.env.openExternal(vscode.Uri.parse('https://aka.ms/vscode-remote/wsl/install-wsl'));
-				} else if (response === installWSL) {
-					telemetry.reportDialog(Dialog.wslNotInstalled, 'install');
-					startWSLInstall(powershellLocation!);
+		return vscode.commands.registerCommand(
+			CommandId.connectToWSL,
+			async () => {
+				telemetry.reportCommand(Command.connectToWSL);
+				const isWSLInstalled = await checkIfWSLInstalled();
+				if (isWSLInstalled !== true) {
+					telemetry.reportDialog(Dialog.wslNotInstalled, "show");
+					const installWSL = "Install Now";
+					const learnMore = "Learn More";
+					const powershellLocation = getPowershellLocation();
+					const buttons =
+						hasWSLInstall() && powershellLocation
+							? [installWSL, learnMore]
+							: [learnMore];
+					const response = await vscode.window.showErrorMessage(
+						localize(
+							"installWSL",
+							"The Windows Subsystem for Linux (WSL) must be installed to complete the action. WSL lets you run a GNU/Linux environment directly on Windows without the overhead of a traditional virtual machine. VS Code, through the WSL extension, can then open folders and run commands, extensions, and the terminal in WSL."
+						),
+						...buttons
+					);
+					if (response === learnMore) {
+						telemetry.reportDialog(Dialog.wslNotInstalled, "open");
+						await vscode.env.openExternal(
+							vscode.Uri.parse(
+								"https://aka.ms/vscode-remote/wsl/install-wsl"
+							)
+						);
+					} else if (response === installWSL) {
+						telemetry.reportDialog(
+							Dialog.wslNotInstalled,
+							"install"
+						);
+						startWSLInstall(powershellLocation!);
+					} else {
+						telemetry.reportDialog(Dialog.wslNotInstalled, "close");
+					}
 				} else {
-					telemetry.reportDialog(Dialog.wslNotInstalled, 'close');
+					telemetry.reportDialog(
+						Dialog.wslRemoteNotInstalled,
+						"show"
+					);
+					const showExtension = localize(
+						"showExtensionButton",
+						"Show Extension"
+					);
+					const res = await vscode.window.showInformationMessage(
+						localize(
+							"installRemoteWSLDescription",
+							"The 'WSL' extension is required to complete the action. The extension allows to open a window where commands, extensions and the terminal run in the Linux subsystem."
+						),
+						showExtension
+					);
+					if (res === showExtension) {
+						telemetry.reportDialog(
+							Dialog.wslRemoteNotInstalled,
+							"open"
+						);
+						await vscode.commands.executeCommand(
+							"workbench.extensions.action.showExtensionsWithIds",
+							[ExtensionId.remoteWSL]
+						);
+					} else {
+						telemetry.reportDialog(
+							Dialog.wslRemoteNotInstalled,
+							"close"
+						);
+					}
 				}
-			} else {
-				telemetry.reportDialog(Dialog.wslRemoteNotInstalled, 'show');
-				const showExtension = localize('showExtensionButton', 'Show Extension');
-				const res = await vscode.window.showInformationMessage(localize('installRemoteWSLDescription', 'The \'WSL\' extension is required to complete the action. The extension allows to open a window where commands, extensions and the terminal run in the Linux subsystem.'), showExtension);
-				if (res === showExtension) {
-					telemetry.reportDialog(Dialog.wslRemoteNotInstalled, 'open');
-					await vscode.commands.executeCommand('workbench.extensions.action.showExtensionsWithIds', [ExtensionId.remoteWSL]);
-				} else {
-					telemetry.reportDialog(Dialog.wslRemoteNotInstalled, 'close');
-				}
+				// Start-Process wsl.exe -- -Verb runAs
 			}
-			// Start-Process wsl.exe -- -Verb runAs
-		});
+		);
 	});
 
-	addFeature(Experiment.openWSLDocumentation, ContextKey.gettingStarted, () => {
-		return vscode.commands.registerCommand(CommandId.gettingStarted, async () => {
-			telemetry.reportCommand(Command.openWSLDocumentation);
-			return vscode.env.openExternal(vscode.Uri.parse(WSL_DOC_URL));
-		});
-	});
+	addFeature(
+		Experiment.openWSLDocumentation,
+		ContextKey.gettingStarted,
+		() => {
+			return vscode.commands.registerCommand(
+				CommandId.gettingStarted,
+				async () => {
+					telemetry.reportCommand(Command.openWSLDocumentation);
+					return vscode.env.openExternal(
+						vscode.Uri.parse(WSL_DOC_URL)
+					);
+				}
+			);
+		}
+	);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() { }
+export function deactivate() {}
 
 export async function checkIfWSLInstalled(): Promise<true | string> {
 	if (getWindowsBuildNumber() >= 22000) {
 		const wslExePath = getWSLExecutablePath();
 		if (wslExePath) {
-			if (!await fileExists(wslExePath)) {
+			if (!(await fileExists(wslExePath))) {
 				return `'${wslExePath}' not found`;
 			}
-			return new Promise<true | string>(s => {
-				cp.execFile(wslExePath, ['--status'], (err) => {
+			return new Promise<true | string>((s) => {
+				cp.execFile(wslExePath, ["--status"], (err) => {
 					if (err) {
-						s('--status exits with error');
+						s("--status exits with error");
 					} else {
 						s(true);
 					}
@@ -150,28 +223,49 @@ function hasWSLInstall() {
 }
 
 function getLxssManagerDllPath(): string | undefined {
-	const is32ProcessOn64Windows = process.env.hasOwnProperty('PROCESSOR_ARCHITEW6432');
-	const systemRoot = process.env['SystemRoot'];
+	const is32ProcessOn64Windows = process.env.hasOwnProperty(
+		"PROCESSOR_ARCHITEW6432"
+	);
+	const systemRoot = process.env["SystemRoot"];
 	if (systemRoot) {
-		return path.join(systemRoot, is32ProcessOn64Windows ? 'Sysnative' : 'System32', 'lxss', 'LxssManager.dll');
+		return path.join(
+			systemRoot,
+			is32ProcessOn64Windows ? "Sysnative" : "System32",
+			"lxss",
+			"LxssManager.dll"
+		);
 	}
 	return undefined;
 }
 
 function getWSLExecutablePath(): string | undefined {
-	const is32ProcessOn64Windows = process.env.hasOwnProperty('PROCESSOR_ARCHITEW6432');
-	const systemRoot = process.env['SystemRoot'];
+	const is32ProcessOn64Windows = process.env.hasOwnProperty(
+		"PROCESSOR_ARCHITEW6432"
+	);
+	const systemRoot = process.env["SystemRoot"];
 	if (systemRoot) {
-		return path.join(systemRoot, is32ProcessOn64Windows ? 'Sysnative' : 'System32', 'wsl.exe');
+		return path.join(
+			systemRoot,
+			is32ProcessOn64Windows ? "Sysnative" : "System32",
+			"wsl.exe"
+		);
 	}
 	return undefined;
 }
 
-function getPowershellLocation() : string | undefined {
-	const is32ProcessOn64Windows = process.env.hasOwnProperty('PROCESSOR_ARCHITEW6432');
-	const systemRoot = process.env['SystemRoot'];
+function getPowershellLocation(): string | undefined {
+	const is32ProcessOn64Windows = process.env.hasOwnProperty(
+		"PROCESSOR_ARCHITEW6432"
+	);
+	const systemRoot = process.env["SystemRoot"];
 	if (systemRoot) {
-		return path.join(systemRoot, is32ProcessOn64Windows ? 'Sysnative' : 'System32', 'WindowsPowerShell', 'v1.0', 'powershell.exe');
+		return path.join(
+			systemRoot,
+			is32ProcessOn64Windows ? "Sysnative" : "System32",
+			"WindowsPowerShell",
+			"v1.0",
+			"powershell.exe"
+		);
 	}
 	return undefined;
 }
@@ -179,17 +273,24 @@ function getPowershellLocation() : string | undefined {
 function startWSLInstall(powershellLocation: string) {
 	const command = [];
 	command.push(powershellLocation);
-	command.push('Start-Process');
-	command.push('-FilePath', 'cmd');
-	command.push('-ArgumentList', `'/c "${getWSLExecutablePath()} --install & pause"'`);
-	command.push('-Verb', 'RunAs');
-	cp.exec(command.join(' '), { encoding: 'utf-8' }, (error, stdout, stderr) => {
-		if (error) {
-			console.log(error);
+	command.push("Start-Process");
+	command.push("-FilePath", "cmd");
+	command.push(
+		"-ArgumentList",
+		`'/c "${getWSLExecutablePath()} --install & pause"'`
+	);
+	command.push("-Verb", "RunAs");
+	cp.exec(
+		command.join(" "),
+		{ encoding: "utf-8" },
+		(error, stdout, stderr) => {
+			if (error) {
+				console.log(error);
+			}
+			console.log(stdout);
+			console.error(stderr);
 		}
-		console.log(stdout);
-		console.error(stderr);
-	});
+	);
 }
 
 async function fileExists(location: string) {
@@ -199,8 +300,8 @@ async function fileExists(location: string) {
 let windowsBuildNumber: number | undefined;
 
 function getWindowsBuildNumber(): number {
-	if (typeof windowsBuildNumber !== 'number') {
-		const osVersion = (/(\d+)\.(\d+)\.(\d+)/g).exec(os.release());
+	if (typeof windowsBuildNumber !== "number") {
+		const osVersion = /(\d+)\.(\d+)\.(\d+)/g.exec(os.release());
 		if (osVersion && osVersion.length === 4) {
 			windowsBuildNumber = parseInt(osVersion[3]);
 		} else {
--- Source/telemetry.ts
+++ Source/telemetry.ts
@@ -2,57 +2,79 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *--------------------------------------------------------------------------------------------*/
 
-import * as fs from 'fs';
-import * as path from 'path';
-import TelemetryReporter from '@vscode/extension-telemetry';
+import * as fs from "fs";
+import * as path from "path";
+import TelemetryReporter from "@vscode/extension-telemetry";
 
-import * as vscode from 'vscode';
-import { getExperimentationService, TargetPopulation, IExperimentationTelemetry, IExperimentationService } from 'vscode-tas-client';
-import { ExtensionId } from './extension';
+import * as vscode from "vscode";
+import {
+	getExperimentationService,
+	TargetPopulation,
+	IExperimentationTelemetry,
+	IExperimentationService,
+} from "vscode-tas-client";
+import { ExtensionId } from "./extension";
 
 enum ConfigKeys {
-	enableTelementry = 'telemetry.enableTelemetry',
-	enableExperiments = 'workbench.enableExperiments',
-	enableAllExperiments = 'remote.WSLRecommender.allExperiments',
+	enableTelementry = "telemetry.enableTelemetry",
+	enableExperiments = "workbench.enableExperiments",
+	enableAllExperiments = "remote.WSLRecommender.allExperiments",
 }
 
 export function enableTelemetry(): boolean {
-	return vscode.workspace.getConfiguration().get(ConfigKeys.enableTelementry) !== false;
+	return (
+		vscode.workspace.getConfiguration().get(ConfigKeys.enableTelementry) !==
+		false
+	);
 }
 
 export function enableExperiments(): boolean {
-	return vscode.workspace.getConfiguration().get(ConfigKeys.enableExperiments) !== false;
+	return (
+		vscode.workspace
+			.getConfiguration()
+			.get(ConfigKeys.enableExperiments) !== false
+	);
 }
 
 export function enableAllExperimentalFeatures(): boolean {
-	return vscode.workspace.getConfiguration().get(ConfigKeys.enableAllExperiments) === true;
+	return (
+		vscode.workspace
+			.getConfiguration()
+			.get(ConfigKeys.enableAllExperiments) === true
+	);
 }
 
 export enum Experiment {
-	connectToWSL = 'connectToWSL',
-	openWSLDocumentation = 'openWSLDocumentation'
+	connectToWSL = "connectToWSL",
+	openWSLDocumentation = "openWSLDocumentation",
 }
 
 export enum Command {
-	connectToWSL = 'connectToWSL',
-	openWSLDocumentation = 'openWSLDocumentation'
+	connectToWSL = "connectToWSL",
+	openWSLDocumentation = "openWSLDocumentation",
 }
 
 export enum Dialog {
-	wslNotInstalled = 'wslNotInstalled',
-	wslRemoteNotInstalled = 'wslRemoteNotInstalled',
+	wslNotInstalled = "wslNotInstalled",
+	wslRemoteNotInstalled = "wslRemoteNotInstalled",
 }
 
 let telemetry: WSLRemoteTelemetry | undefined = undefined;
 
-export function getTelemetry(context: vscode.ExtensionContext): WSLRemoteTelemetry {
+export function getTelemetry(
+	context: vscode.ExtensionContext
+): WSLRemoteTelemetry {
 	if (telemetry) {
 		return telemetry;
 	}
 
-	const wslExtension = vscode.extensions.getExtension(ExtensionId.remoteWSLRecommender);
+	const wslExtension = vscode.extensions.getExtension(
+		ExtensionId.remoteWSLRecommender
+	);
 	if (!wslExtension) {
-		throw new Error(`${ExtensionId.remoteWSLRecommender} not found in extensions.`);
+		throw new Error(
+			`${ExtensionId.remoteWSLRecommender} not found in extensions.`
+		);
 	}
 	const extensionPackage = wslExtension.packageJSON;
 
@@ -74,23 +96,34 @@ export function getTelemetry(context: vscode.ExtensionContext): WSLRemoteTelemet
 					"ABExp.queriedFeature": { "classification": "SystemMetaData", "purpose": "FeatureInsight" }
 				}
 			*/
-			experimentService = getExperimentationService(`${publisher}.${name}`, version, target, reporter, context.globalState);
+			experimentService = getExperimentationService(
+				`${publisher}.${name}`,
+				version,
+				target,
+				reporter,
+				context.globalState
+			);
 		} else {
 			experimentService = undefined;
 		}
 	};
 	initExperimentationService();
 
-	context.subscriptions.push(vscode.workspace.onDidChangeConfiguration(e => {
-		if (e.affectsConfiguration(ConfigKeys.enableTelementry) || e.affectsConfiguration(ConfigKeys.enableExperiments)
-			|| e.affectsConfiguration(ConfigKeys.enableAllExperiments)) {
+	context.subscriptions.push(
+		vscode.workspace.onDidChangeConfiguration((e) => {
+			if (
+				e.affectsConfiguration(ConfigKeys.enableTelementry) ||
+				e.affectsConfiguration(ConfigKeys.enableExperiments) ||
+				e.affectsConfiguration(ConfigKeys.enableAllExperiments)
+			) {
 				initExperimentationService();
 				onDidChangeEmitter.fire();
-		}
-	}));
+			}
+		})
+	);
 
 	telemetry = {
-		reportDialog(kind: Dialog, outcome: 'open' | 'close' | string): void {
+		reportDialog(kind: Dialog, outcome: "open" | "close" | string): void {
 			if (!enableTelemetry()) {
 				return;
 			}
@@ -101,7 +134,7 @@ export function getTelemetry(context: vscode.ExtensionContext): WSLRemoteTelemet
 				}
 			 */
 			const data: Record<string, string> = { kind, outcome };
-			reporter.sendTelemetryEvent('recommendation', data);
+			reporter.sendTelemetryEvent("recommendation", data);
 		},
 		reportCommand(kind: Command): void {
 			if (!enableTelemetry()) {
@@ -113,37 +146,47 @@ export function getTelemetry(context: vscode.ExtensionContext): WSLRemoteTelemet
 				}
 			 */
 			const data: Record<string, string> = { kind };
-			reporter.sendTelemetryEvent('command', data);
+			reporter.sendTelemetryEvent("command", data);
 		},
 		async isExperimentEnabled(experiment: Experiment): Promise<boolean> {
 			if (enableAllExperimentalFeatures()) {
 				return true;
 			}
 
-			return experimentService !== undefined && experimentService.isCachedFlightEnabled(experiment);
+			return (
+				experimentService !== undefined &&
+				experimentService.isCachedFlightEnabled(experiment)
+			);
 		},
-		onDidChange: onDidChangeEmitter.event
+		onDidChange: onDidChangeEmitter.event,
 	};
 	return telemetry;
 }
 
 export interface WSLRemoteTelemetry {
-	reportDialog(kind: Dialog, outcome: 'open' | 'close' | string): void;
+	reportDialog(kind: Dialog, outcome: "open" | "close" | string): void;
 	reportCommand(kind: Command): void;
 	isExperimentEnabled(experiment: Experiment): Promise<boolean>;
 	readonly onDidChange: vscode.Event<void>;
 }
 class ExperimentationTelemetry implements IExperimentationTelemetry {
-
 	private sharedProperties: Record<string, string> = {};
 
-	constructor(private baseReporter: TelemetryReporter) { }
+	constructor(private baseReporter: TelemetryReporter) {}
 
-	sendTelemetryEvent(eventName: string, properties?: Record<string, string>, measurements?: Record<string, number>) {
-		this.baseReporter.sendTelemetryEvent(eventName, {
-			...this.sharedProperties,
-			...properties
-		}, measurements);
+	sendTelemetryEvent(
+		eventName: string,
+		properties?: Record<string, string>,
+		measurements?: Record<string, number>
+	) {
+		this.baseReporter.sendTelemetryEvent(
+			eventName,
+			{
+				...this.sharedProperties,
+				...properties,
+			},
+			measurements
+		);
 	}
 
 	setSharedProperty(name: string, value: string): void {
@@ -162,11 +205,16 @@ class ExperimentationTelemetry implements IExperimentationTelemetry {
 function getTargetPopulation() {
 	const { quality } = getProductConfiguration(vscode.env.appRoot);
 	switch (quality) {
-		case 'stable': return TargetPopulation.Public;
-		case 'insider': return TargetPopulation.Insiders;
-		case 'exploration': return TargetPopulation.Internal;
-		case undefined: return TargetPopulation.Team;
-		default: return TargetPopulation.Public;
+		case "stable":
+			return TargetPopulation.Public;
+		case "insider":
+			return TargetPopulation.Insiders;
+		case "exploration":
+			return TargetPopulation.Internal;
+		case undefined:
+			return TargetPopulation.Team;
+		default:
+			return TargetPopulation.Public;
 	}
 }
 
@@ -179,9 +227,13 @@ export interface IProductConfiguration {
 
 let product: IProductConfiguration;
 
-export function getProductConfiguration(appRoot: string): IProductConfiguration {
+export function getProductConfiguration(
+	appRoot: string
+): IProductConfiguration {
 	if (!product) {
-		const content = fs.readFileSync(path.join(appRoot, 'product.json')).toString();
+		const content = fs
+			.readFileSync(path.join(appRoot, "product.json"))
+			.toString();
 		product = JSON.parse(content) as IProductConfiguration;
 	}
 	return product;
